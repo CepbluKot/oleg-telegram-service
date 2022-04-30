ceb8de2b973c86b3604f4b0ace54e7442d00cea0
Eliss-good <elisssupergames@gmail.com>
2022-04-30 16:13:24 +0300

    add jwt

--- back/admin_tools.py
+++ back/admin_tools.py
@@ -1,4 +1,8 @@
-from main import ModelView, db, admin, flask_app, Migrate
+from main import ModelView, db, admin, flask_app, jwt, get_jwt, verify_jwt_in_request, create_access_token, jwt_required
+from functools import wraps
+
+from flask import jsonify, request
+
 
 from models.days_coonecta import Days
 from models.all_users_this_connecta import CompanyUsers
@@ -14,3 +18,39 @@ admin.add_view(ModelView(MyService, db.session))
 admin.add_view(ModelView(MyStaff, db.session))
 admin.add_view(ModelView(UsersConnectALL, db.session))
 admin.add_view(ModelView(AllBooking, db.session))
+
+
+def admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims["is_administrator"]:
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(msg="Admins only!"), 403
+
+        return decorator
+
+    return wrapper
+
+
+@flask_app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(
+        "admin_user", additional_claims={"is_administrator": True}
+    )
+    return jsonify(access_token=access_token)
+
+
+@flask_app.route("/protected", methods=["GET"])
+@admin_required()
+def protected():
+    return jsonify(foo="bar")
+
--- back/main.py
+++ back/main.py
@@ -7,6 +7,12 @@ from flask_marshmallow import Marshmallow
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import verify_jwt_in_request
+from flask_jwt_extended import jwt_required
+
 sys.path.append('/home/eliss/tg-helper/back/models')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -16,6 +22,9 @@ flask_app.config['FLASK_ENV'] = 'development'
 flask_app.config['SECRET_KEY'] = 'pop'
 flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:admin@localhost/system_control'
+
+flask_app.config['"JWT_SECRET_KEY'] = 'key-popov-gay'
+jwt = JWTManager(flask_app)
 
 db = SQLAlchemy(flask_app)
 admin = Admin(flask_app)
