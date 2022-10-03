8ed0104aeb2f598b18fdf66e9e3908a0fe2d8149
Eliss-good <elisssupergames@gmail.com>
2022-10-03 23:01:30 +0300

    fix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from setting_web import head_conf, Migrate
-migrate = Migrate(head_conf.flask_app, head_conf.db)
+from setting_web import flask_app, db, Migrate
+migrate = Migrate(flask_app, db)
 
 from back_users.models.all_models import *
 from back.models.booking_models import *
